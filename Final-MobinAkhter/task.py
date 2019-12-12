6b9c69167d0beeaa37313d1f1a7137d756da976d
mobin <mobinakhter18@gmail.com>
2019-12-12 13:46:03 -0500

    Exception handlind done partly.

--- Final-MobinAkhter/task.py
+++ Final-MobinAkhter/task.py
@@ -1,6 +1,27 @@
-from researchTask import ResearchTask
-from designTask import DesignTask
-from implementationTask import ImplementationTask
 class Task:
+    def __init__(self):
+        self._taskDescription =  _taskDescription
+        self._highRisk = False
+        self._duration = 3
+    def getTaskDescription(self):
+        return self.taskDescription
+    def setTaskDescription(self, newTaskDescription):
+        self._taskDescription = newTaskDescription
+    def getHighRisk(self):
+        return self.highRisk
+    def setHighRisk(self, isHighRisk):
+        self._highRisk = isHighRisk
+
+    def getDuration(self):
+        return self._duration
+    def setDuration(self, newDuration):
+        self._duration = newDuration
     def determineEstimation(self):
         pass
+    class TaskError(Exception):
+        "This class will be used when the user has an incorrect input."
+
+
+
+        
+    
