766d63f3c18a1c50c82d604ac26bbf46afb7074a
mobin <mobinakhter18@gmail.com>
2019-12-12 21:26:54 -0500

    the user can now add/"create" books.

--- CRUD Assignment 4 & 5/application.py
+++ CRUD Assignment 4 & 5/application.py
@@ -7,5 +7,77 @@ class Application(Frame,CRUD):
         Frame.__init__(self, master)
         self.master = master
         self.window(100,10)
-        
+    
+    # Creating the gui window
+    def window(mainWindow, row=None, col=None):
+        mainWindow.master.title("Library") # Changes the "Tk" symbol at the top of the gui window to "Library"
 
+        mainWindow.pack(fill=BOTH, expand=1)
+
+        # Creating "buttons" inside the window
+        # First paramater is for the location where you want to create the button.
+        # Second parameter is for the specific text you want to appear on the gui window
+        # Third parameter is for making the button access a method which does further operations.
+        # Fourth parameter  is for the background color 
+        # Fifth parameter is for the foreground color.
+        addBookButton = Button(mainWindow, text="Add a book to your library", command=mainWindow.addBook, bg="blue", fg="white")
+        searchSpecificBook = Button(mainWindow, text="Search for a Book", command=mainWindow.searchABook, bg="purple", fg="white")
+        deleteABook = Button(mainWindow, text="Delete a book ", command=mainWindow.deleteBook, bg="yellow", fg="black")
+        searchBookHistory = Button(mainWindow, text="Book record", command=mainWindow.bookHistory, bg="green", fg="black")
+        quitButton = Button(mainWindow, text="Exit", command=mainWindow.exit, bg="red" , fg="white" )
+
+        addBookButton.pack()
+        addBookButton.place(x=0, y=0)
+        addBookButton.pack(fill=X)
+
+        searchSpecificBook.pack()
+        searchSpecificBook.place(x=1, y=0)
+        searchSpecificBook.pack(fill=X)
+
+        deleteABook.pack()
+        deleteABook.place(x=2, y=0)
+        deleteABook.pack(fill=X)
+
+        searchBookHistory.pack()
+        searchBookHistory.place(x=3, y=0)
+        searchBookHistory.pack(fill=X)
+
+        quitButton.pack()
+        quitButton.place(x=4, y=0)
+        quitButton.pack(fill=X)
+
+        menu = Menu(mainWindow.master)
+        mainWindow.master.config(menu=menu)
+
+        file = Menu(menu)
+
+        menu.add_cascade(label="File", menu=file)
+
+        edit = Menu(menu)
+
+        edit.add_command(label = "Undo")
+
+        menu.add_cascade(label="Edit", menu=edit)
+
+    def exit(self):
+        print("Good-Bye!")
+    
+    def bookHistory(self):
+        with open("data.json", "r") as f:
+            data = f.read()
+            json_data = json.loads(data)
+        print("|=================================|")
+        print("|", "{:<20} {:<!5} {:<10}".format("Book", "Author", "Price", "|"))
+        print("|=================================|")
+        for k, v in sorted(json_data.items()):
+            book, author, price = v.values()
+            print("|", "{:<20} {:<15} {:<10}".format(book, author, price), "|")
+        print("|=================================|")
+        return True
+
+    def addBook(self):
+        book = input("Add book: ")
+        CRUD.create(self,book)
+        
+        
+        
--- CRUD Assignment 4 & 5/crud.py
+++ CRUD Assignment 4 & 5/crud.py
@@ -5,4 +5,36 @@ class CRUD:
     def __init__(self):
         self.book_name = None
 
-    
+    def create(self, book_name):
+        if book_name not in lib:
+            try:
+                author_name= input("Author name: ")
+                if len(author_name) ==0 or author_name.strip() == "":
+                    # Line of code above basically checks if the user did not enter anything
+                    raise ValueError
+                price = input("Book price: ")
+                if len(price) ==0 or price.strip == "":
+                    raise ValueError
+            except:
+                print("ValueError : You did not enter anything ")
+                return False
+            
+            lib[book_name] = {"name": book_name, "author": author_name, "price": "$ " + price}
+            j = json.dumps(lib)
+            f= open("data.json", "w") # w => writes to file
+            f.write(j)
+            print("Your", book_name, "has been added to the list in the library")
+            f.close()
+            return True
+        else:
+            print(book_name, " : already exists in the library.")
+            opt = input("Do you want to add books again? Enter y/Y or n/N")
+            if opt.lower() == "y" or opt.lower() == "yes":
+                book_name = input("Name of the book you want to add: ")
+                CRUD.create(self, book_name)
+            elif opt.lower() == "n" or opt.lower() == "no":
+                return False
+            else:
+                print("Not a valid entry")
+                raise ValueError
+            
